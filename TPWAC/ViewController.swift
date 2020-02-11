8039b6e581d0617ade2f536cb86dcffa94186545
AtomaBirdo <jwang@trinitypawling.org>
2020-02-11 14:59:30 -0500

    Added two new variable (Due day and location) into Activity Class Partially finished adding new activity into the list

--- TPWAC/ViewController.swift
+++ TPWAC/ViewController.swift
@@ -14,7 +14,6 @@ var ref : DatabaseReference = Database.database().reference()
 
 class ViewController: UIViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
